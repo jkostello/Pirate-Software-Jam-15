4976742b598e468e6680ae26988e6000ac1cbb8d
TheFinnigan <nathanpegs@gmail.com>
2024-07-21 17:52:37 -0500

    various progess

--- Circles/circle.gd
+++ Circles/circle.gd
@@ -1,12 +1,35 @@
 extends Node2D
+class_name RitualCircle
 
+@export var identifier := ""
 
-@export var ingredient_points : Array
-
-
+var base_ingredient : PackedScene = preload("res://Ingredients/base_ingredient.tscn")
+var ingredients := {}
 
 # Trigger this when dust is sprinkled
 # This should lock the ingredients, play an animation, clear everything, then make the item
-func complete_ritual():
-	pass
+func complete_ritual(dust_id : String):
+	var points := $IngredientPoints.get_children()
 	
+	if ingredients.has(points[0]):
+		ingredients[points[0]]
+	
+	match identifier:
+		"Bigram":
+			pass
+		"Pentagram":
+			pass
+		_:
+			print("identifier not recognized:  ", identifier)
+	
+	clear_circle()
+	
+	var output := base_ingredient.instantiate()
+	
+	get_parent().add_child(output)
+
+
+func clear_circle():
+	for i in ingredients:
+		ingredients[i].queue_free()
+		ingredients.clear()
--- Ingredients/ingredient.gd
+++ Ingredients/ingredient.gd
@@ -2,15 +2,28 @@ extends Node2D
 class_name Ingredient
 
 @export var is_dust := false
+@export var is_chalk := false
+@export var identifier := ""
 
 var source := true # Used if we have the ingredient itself on the shelf
 var hovered := false
-var following_mouse := false
+var following_mouse := false : 
+	set(v):
+		if v:
+			global_position = get_global_mouse_position()
+		following_mouse = v
 var click_pos := Vector2.ZERO
+var current_point : Area2D
+
 
 func _ready():
 	$Area2D.set_collision_mask_value(3, not is_dust)
-	$Area2D.set_collision_mask_value(4, is_dust)
+	$Area2D.set_collision_mask_value(4, is_dust or is_chalk)
+	
+	if not OS.is_debug_build():
+		$ClickableArea/DevVisual.queue_free()
+	else:
+		$ClickableArea/DevVisual/Label.text = identifier
 
 
 func _process(delta):
@@ -29,7 +42,7 @@ func _input(event):
 				drop()
 			else:
 				if event.pressed:
-					%ClickTimer.start()
+					%ClickTimer.start() # ClickTimer allows for clicking instead of dragging
 					click_pos = get_global_mouse_position()
 				else:
 					if not %ClickTimer.is_stopped():
@@ -47,22 +60,36 @@ func pick_up():
 		new_ingredient.source = false
 	else:
 		following_mouse = true
+		
+		if current_point:
+			current_point.get_parent().get_parent().ingredients.erase(current_point)
+			current_point = null
 
 
 func drop():
 	following_mouse = false
 	if not $Area2D.has_overlapping_areas():
-		queue_free()
-		pass # Add a vanishing effect or something due to invalid placement (maybe in separate function)
+		fail_placement()
 	else:
 		var area : Area2D = $Area2D.get_overlapping_areas()[0]
 		
-		if is_dust:
-			area.get_parent().complete_ritual()
+		if is_chalk:
+			area.get_parent().clear_circle()
+		elif is_dust:
+			area.get_parent().complete_ritual(identifier)
+			queue_free()
 		else:
-			# This can be changed depending on if we want it to snap on drop or while held 
-			# Also needs to check the circle to see if the spot is taken (maybe swap held if so)
-			global_position = area.global_position
+			if area.get_parent().get_parent().ingredients.has(area): # Checks if the spot is taken
+				fail_placement()
+			else:
+				current_point = area
+				global_position = area.global_position
+				area.get_parent().get_parent().ingredients[area] = self
+
+
+func fail_placement():
+	queue_free()
+	# TODO: Add a vanishing effect or something
 
 
 func _on_clickable_area_mouse_entered():
